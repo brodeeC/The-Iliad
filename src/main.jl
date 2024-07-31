4a2702100b892681ae2d4245d149f7ace1b744de
brodeeC <brodeeclontz@gmail.com>
2024-07-31 12:48:50 -0400

    added all files

---
+++ src/main.jl
@@ -0,0 +1,66 @@
+using Genie
+using Genie.Router
+using Genie.Renderer.Html
+using Genie, Genie.Requests
+using HTTP
+
+# Route for serving static files like index.html
+route("/", method = GET) do
+    try
+        html_content = read(joinpath(@__DIR__, "public", "index.html"), String)
+        return HTTP.Response(200, [("Content-Type", "text/html")], html_content)
+    catch e
+        return HTTP.Response(500, "Error rendering HTML view: $e")
+    end
+end
+
+# Route for redirecting to the first page of Book1
+route("/startReading") do
+    try
+        path = read(joinpath(@__DIR__,"public/Book1/page1.html"), String)
+        return HTTP.Response(200, [("Content-Type", "text/html")], path)
+    catch e
+        return HTTP.Response(500, "Error rendering HTML view: $e")
+    end
+end
+
+route("/search", method = POST) do 
+    try
+        search_query = jsonpayload()["query"]
+        
+        if isempty(search_query)
+            return HTTP.Response(400, "Search query cannot be empty")
+        end
+        
+        # Get both numbers into a vector
+        query = split(search_query, ".")
+
+        # Get the book number from the first element of the vector
+        bookNum = parse(Int64, query[1])
+
+        # Get the page number as a String, parse it into an integer
+        # Mod it by 25 to get the page number
+        pageNum = parse(Int64, query[2]) % 25
+        
+        current_book_dir = joinpath(@__DIR__, "public", "Book$(bookNum)")
+        page_path = joinpath(current_book_dir, "page$(pageNum).html")
+        
+        HTML_content = read(page_path, String)
+        return HTTP.Response(200, [("Content-Type", "text/html")], HTML_content)
+    catch err
+        @error "Error processing request: $err"
+        return HTTP.Response(500, "Error processing request: $err")
+    end
+end
+
+# Route for serving Iliad book pages
+route("/:book/:page", method = GET) do
+
+    # Correct path to the book's directory
+    current_book_dir = joinpath(@__DIR__, "public", "Book$(payload(:book))")
+    page_path = joinpath(current_book_dir, "page$(payload(:page)).html")
+    
+    HTML_content = read(page_path, String)
+    return HTTP.Response(200, [("Content-Type", "text/html")], HTML_content)
+   
+end
