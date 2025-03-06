da001eda64605621af581b458660bee977c82e9f
brodeeC <brodeeclontz@gmail.com>
2025-03-06 15:14:03 -0500

    up

--- start_server.jl
+++ start_server.jl
@@ -11,7 +11,7 @@ include("config/routes.jl")
 # Start the server
 try
     @info "Starting Genie server..."
-    Genie.up(port = 8000)
+    Genie.up(port = 8000, host="5.161.56.64")
 catch e
     @error "Error starting Genie server: $e"
     throw(e)
